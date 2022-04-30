9d1136f4a35d88099c23619440a5930f2e0c2834
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-30 10:14:29 +0300

    Feat: Adds Articles and Sources models

---
+++ app/models.py
@@ -0,0 +1,22 @@
+class Articles:
+    '''
+    News Articles class to define news articles objects
+    '''
+    def __init__(self,title,urlToImage,content,author,publishedAt,url):
+        self.title = title
+        self.urlToImage = urlToImage
+        self.content = content
+        self.author = author
+        self.publishedAt= publishedAt
+        self.url=url
+
+class Sources:
+    '''
+    News Sources class to define news source objects
+    '''
+
+    def __init__(self,id,name,category,description):
+        self.id = id
+        self.name = name
+        self.category = category
+        self.description = description
