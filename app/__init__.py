b504847726fa1b55ce5f6bb5c2a8313ab8b6fc68
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-04-30 10:09:13 +0300

    Feat: Adds and registers blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -5,14 +5,17 @@ from config import config_options
 
 bootstrap = Bootstrap()
 
+app = Flask(__name__)
 
 def create_app(config_name):
-
-    app = Flask(__name__)
 
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
 
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
     # Initializing flask extensions
     bootstrap.init_app(app)
 
