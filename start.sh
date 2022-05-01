66c633660d8060ae022eda35c0111d61f1bc1f0c
Williamsoditi <williams.oditi@student.moringaschool.com>
2022-05-01 21:09:30 +0300

    Fix : Changes identifier in start.sh file and downgrades packages

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export NEWS_API_KEY = 'bb900c6b52db42d0a4cd6a8b422b91a9'
-export SECRET_KEY = 'qwerty12345'
+export NEWS_API_KEY='bb900c6b52db42d0a4cd6a8b422b91a9'
+export SECRET_KEY='qwerty12345'
 
 python3 manage.py server
